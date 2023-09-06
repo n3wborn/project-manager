c4521df98fd221dd5b2aa07e5a4c1b586af5c0f5
n3wborn <n3wborn@protonmail.com>
2023-09-06 18:40:51 +0200

    feat: add Project DTO,Mapper

---
+++ src/Service/Project/DTO.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service\Project;
+
+final class DTO
+{
+    public function __construct(
+        private string $slug,
+        private string $name,
+        private string $description,
+    ) {
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ src/Service/Project/Mapper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service\Project;
+
+use App\Entity\Project;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+final class Mapper
+{
+    /** @throws ExceptionInterface */
+    public static function fromEntityToJson(Project $project): mixed
+    {
+        $dto = self::fromEntityToDTO($project);
+        $serializer = new Serializer([new ObjectNormalizer()]);
+
+        return $serializer->normalize($dto, JsonEncoder::FORMAT);
+    }
+
+    public static function fromEntityToDTO(Project $project): DTO
+    {
+        return new DTO(
+            $project->getSlug(),
+            $project->getName(),
+            $project->getDescription(),
+        );
+    }
+}
