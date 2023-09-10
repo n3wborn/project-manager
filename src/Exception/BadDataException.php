ba374202ac1b11deaeeeb960c49186839df0745f
n3wborn <n3wborn@protonmail.com>
2023-09-11 01:36:16 +0200

    feat(BadDataException): add default message, status code

--- src/Exception/BadDataException.php
+++ src/Exception/BadDataException.php
@@ -2,6 +2,12 @@
 
 namespace App\Exception;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class BadDataException extends ProjectManagerException
 {
+    public function __construct(string $message = '')
+    {
+        parent::__construct($message, Response::HTTP_BAD_REQUEST);
+    }
 }
