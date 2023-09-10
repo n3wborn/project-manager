2071b18e5f32529a12818ee61385cb9570f7bdb1
n3wborn <n3wborn@protonmail.com>
2023-09-11 01:44:09 +0200

    feat: add Archiver

---
+++ src/Service/Project/Archiver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service\Project;
+
+use App\Entity\Project;
+use App\Exception\BadDataException;
+use App\Exception\NotFoundException;
+use App\Helper\ApiMessages;
+use App\Helper\ApiResponse;
+use App\Helper\ExceptionLogger;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+final class Archiver
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private Validator $validator,
+        private Mapper $mapper,
+        private ExceptionLogger $logger,
+    ) {
+    }
+
+    public function softDelete(Project $project): void
+    {
+        $this->entityManager->persist($project->setArchivedAt(new \DateTimeImmutable()));
+        $this->entityManager->flush();
+    }
+
+    public function process(?Project $project): JsonResponse
+    {
+        try {
+            $this->validator->validateProjectIsArchivable($project);
+            $this->softDelete($project);
+
+            $response = ApiResponse::createAndFormat(
+                Mapper::fromEntityToJson($project),
+                ApiMessages::PROJECT_DELETE_SUCCESS_MESSAGE);
+        } catch (NotFoundException|BadDataException $exception) {
+            $this->logger->logNotice($exception);
+            $response = ApiResponse::createWarningMessage($exception->getMessage());
+        } catch (\Throwable $exception) {
+            $this->logger->logAndTrace($exception);
+            $response = ApiResponse::createErrorMessage(ApiMessages::DEFAULT_ERROR_MESSAGE, exception: $exception);
+        }
+
+        return $response;
+    }
+}
