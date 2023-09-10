6423c2a8228b6298fe52600023dae9684cf8da35
n3wborn <n3wborn@protonmail.com>
2023-09-11 01:42:04 +0200

    refac(Controller): use MapRequestPayload for create/update function

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Service\Project\DTO;
 use App\Service\Project\Handler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class ProjectController extends AbstractController
@@ -22,22 +24,31 @@ final class ProjectController extends AbstractController
     public const ROUTE_FETCH = 'project_fetch';
     public const ROUTE_FIND_ALL = 'project_find_all';
 
-    #[Route('/project/${project}', name: self::ROUTE_FETCH, methods: Request::METHOD_GET)]
-    public function getProject(Project $project): JsonResponse
+    #[Route('/project/{slug}', name: self::ROUTE_FETCH, methods: Request::METHOD_GET)]
+    public function getProject(?Project $project): JsonResponse
     {
         return $this->handler->handleGetProject($project);
     }
 
     #[Route('/project', name: self::ROUTE_ADD, methods: Request::METHOD_POST)]
-    #[Route('/project/${project}', name: self::ROUTE_EDIT, methods: Request::METHOD_POST)]
-    public function persistProject(?Project $project, Request $request): JsonResponse
-    {
-        return $this->handler->handlePersistProject($project, $request);
+    #[Route('/project/{slug}', name: self::ROUTE_EDIT, methods: Request::METHOD_POST)]
+    public function persistProject(
+        ?Project $project,
+        #[MapRequestPayload()] ?DTO $dto,
+        Request $request,
+    ): JsonResponse {
+        return $this->handler->handlePersistProject($project, $request, $dto);
     }
 
     #[Route('/projects', name: self::ROUTE_FIND_ALL, methods: Request::METHOD_GET)]
     public function getAllProjects(): JsonResponse
     {
         return $this->handler->handleGetAllProjects();
+    }
+
+    #[Route('/project/{slug}', name: self::ROUTE_ARCHIVE, methods: 'DELETE')]
+    public function classeArchive(?Project $project): JsonResponse
+    {
+        return $this->handler->handleArchiveProject($project);
     }
 }
