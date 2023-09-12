1853b159621f1489494d4f4c6da0790976798d58
n3wborn <n3wborn@protonmail.com>
2023-09-12 17:37:27 +0200

    feat: add Category entity

---
+++ src/Repository/CategoryRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Category;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Category>
+ *
+ * @method Category|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Category|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Category[]    findAll()
+ * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CategoryRepository extends ServiceEntityRepository
+{
+    public const ENTITY_ALIAS = 'c';
+
+    private ?QueryBuilder $qb = null;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Category::class);
+    }
+
+    public function save(Category $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+        $flush && $this->getEntityManager()->flush();
+    }
+
+    public function remove(Category $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+        $flush && $this->getEntityManager()->flush();
+    }
+
+    private function getQueryBuilder(): QueryBuilder
+    {
+        return $this->qb ?: $this->createQueryBuilder(self::ENTITY_ALIAS);
+    }
+
+    public function findAllNotArchived(): array
+    {
+        return ($qb = $this->getQueryBuilder())
+            ->where($qb->expr()->isNull('c.archivedAt'))
+            ->orderBy('c.name', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    //    /**
+    //     * @return Category[] Returns an array of Category objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Category
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
