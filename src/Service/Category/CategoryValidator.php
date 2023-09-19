294ea5374b1025ea8254d63d9bd515d93a658df4
n3wborn <n3wborn@protonmail.com>
2023-09-19 10:38:38 +0200

    feat(Category): add Validator
    see: #14

---
+++ src/Service/Category/CategoryValidator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Service\Category;
+
+use App\Entity\Category;
+use App\Exception\BadDataException;
+use App\Exception\NotFoundException;
+use App\Helper\ApiMessages;
+use App\Repository\CategoryRepository;
+
+final class CategoryValidator
+{
+    public const CATEGORY_ALREADY_ARCHIVED = 'La catégorie est déjà archivée';
+    public const NAME_SHOULD_BE_UNIQUE = 'La catégorie doit avoir un nom unique';
+    public const NAME_SHOULD_NOT_BE_EMPTY = 'Le champ Nom ne peut être vide';
+
+    public function __construct(
+        private CategoryRepository $categoryRepository,
+    ) {
+    }
+
+    /** @throws BadDataException*/
+    public function validate(CategoryDTO $dto): void
+    {
+        $this
+            ->validateNameNotEmpty($dto->getName())
+            ->validateNameIsUnique($dto->getName())
+        ;
+    }
+
+    /** @throws BadDataException*/
+    private function validateNameIsUnique(string $name): self
+    {
+        $this->categoryRepository->findBy(['name' => $name])
+            && throw new BadDataException(self::NAME_SHOULD_BE_UNIQUE);
+
+        return $this;
+    }
+
+    /** @throws BadDataException */
+    private function validateNameNotEmpty(string $name): self
+    {
+        empty($name) && throw new BadDataException(self::NAME_SHOULD_NOT_BE_EMPTY);
+
+        return $this;
+    }
+
+    /** @throws BadDataException|NotFoundException */
+    public function validateCategoryIsArchivable(?Category $category): self
+    {
+        return $this
+            ->validateKnownEntity($category)
+            ->isArchivable($category);
+    }
+
+    /** @throws BadDataException */
+    private function isArchivable(Category $category): self
+    {
+        $category->isArchived() && throw new BadDataException(self::CATEGORY_ALREADY_ARCHIVED);
+
+        return $this;
+    }
+
+    /** @throws NotFoundException */
+    private function validateKnownEntity(?Category $category): self
+    {
+        !$category && throw new NotFoundException(ApiMessages::translate(ApiMessages::CATEGORY_UNKNOWN));
+
+        return $this;
+    }
+}
