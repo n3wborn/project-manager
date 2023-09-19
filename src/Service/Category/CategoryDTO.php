2e3ba9340c0c12e5c6a5923636305403f01ba700
n3wborn <n3wborn@protonmail.com>
2023-09-19 10:38:14 +0200

    feat(Category): add DTO/Mapper
    see: #14

---
+++ src/Service/Category/CategoryDTO.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service\Category;
+
+final class CategoryDTO
+{
+    public function __construct(
+        private string $name = '',
+        private ?string $slug = null,
+    ) {
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
---
+++ src/Service/Category/CategoryMapper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service\Category;
+
+use App\Entity\Category;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+final class CategoryMapper
+{
+    /** @throws ExceptionInterface */
+    public static function fromEntityToJson(Category $category): mixed
+    {
+        $dto = self::fromEntityToDTO($category);
+        $serializer = new Serializer([new ObjectNormalizer()]);
+
+        return $serializer->normalize($dto, JsonEncoder::FORMAT);
+    }
+
+    public static function fromEntityToDTO(Category $category): CategoryDTO
+    {
+        return new CategoryDTO(
+            $category->getName(),
+            $category->getSlug(),
+        );
+    }
+}
