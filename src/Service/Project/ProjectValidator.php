ba487967227baabeace2d2cec495b7b260b87bc2
n3wborn <n3wborn@protonmail.com>
2023-10-12 14:58:07 +0200

    feat(projectValidator): add project categories validation methods
    see: #19

--- src/Service/Project/ProjectValidator.php
+++ src/Service/Project/ProjectValidator.php
@@ -6,6 +6,7 @@ use App\Entity\Project;
 use App\Exception\BadDataException;
 use App\Exception\NotFoundException;
 use App\Helper\ApiMessages;
+use App\Repository\CategoryRepository;
 use App\Repository\ProjectRepository;
 
 final class ProjectValidator
@@ -18,6 +19,7 @@ final class ProjectValidator
 
     public function __construct(
         private ProjectRepository $projectRepository,
+        private CategoryRepository $categoryRepository,
     ) {
     }
 
@@ -34,7 +36,6 @@ final class ProjectValidator
     /** @throws BadDataException */
     private function validateCategories(ProjectDTO $dto): self
     {
-        // get related Category entity (?) -> check if exists -> check if
         foreach ($dto->getCategories() as $category) {
             $this->validateCategory($category);
         }
@@ -43,9 +44,58 @@ final class ProjectValidator
     }
 
     /** @throws BadDataException */
-    private function validateCategory(mixed $categorySlug): self
+    private function validateCategory(mixed $data): self
     {
-        !is_string($categorySlug['slug']) && throw new BadDataException(self::CATEGORY_SLUG_INVALID);
+        $this
+            ->validateCategoryFormat($data)
+            ->validateCategoryIsNotEmptyString($data)
+            ->validateCategorySlugIsValidEntity($data);
+
+        return $this;
+    }
+
+    private function validateCategoryFormat(mixed $data): self
+    {
+        (!is_array($data) || !array_key_exists('slug', $data))
+            && throw new BadDataException(self::CATEGORY_SLUG_INVALID);
+
+        return $this;
+    }
+
+    private function validateCategoryIsNotEmptyString(mixed $categorySlug): self
+    {
+        (empty($categorySlug['slug']) || !is_string($categorySlug['slug']))
+            && throw new BadDataException(self::CATEGORY_SLUG_INVALID);
+
+        return $this;
+    }
+
+    /** @throws BadDataException */
+    private function validateCategorySlugIsValidEntity(mixed $data): self
+    {
+        $slug = $data['slug'];
+
+        $this
+            ->validateCategoryExists($slug)
+            ->validateCategoryIsNotArchived($slug);
+
+        return $this;
+    }
+
+    /** @throws BadDataException */
+    private function validateCategoryExists(string $slug): self
+    {
+        (null === $this->categoryRepository->findOneBy(['slug' => $slug]))
+            && throw new NotFoundException(self::CATEGORY_SLUG_INVALID);
+
+        return $this;
+    }
+
+    /** @throws BadDataException */
+    private function validateCategoryIsNotArchived(string $slug): self
+    {
+        (null !== $this->categoryRepository->findOneBy(['slug' => $slug])->getArchivedAt())
+            && throw new NotFoundException(self::CATEGORY_SLUG_INVALID);
 
         return $this;
     }
@@ -94,15 +144,6 @@ final class ProjectValidator
 
         (null !== $existingProject)
             && ($existingProject->getSlug() !== $dto->getSlug())
-            && throw new BadDataException(self::NAME_SHOULD_BE_UNIQUE);
-
-        return $this;
-    }
-
-    /** @throws BadDataException */
-    private function validateNameIsUnique(string $name): self
-    {
-        $this->projectRepository->findBy(['name' => $name, 'archivedAt' => null])
             && throw new BadDataException(self::NAME_SHOULD_BE_UNIQUE);
 
         return $this;
